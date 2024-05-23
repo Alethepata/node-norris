32b20e00efa0d11fad9f0062bfdb505963fab9cf
Alethepata <alessia.patane@gmail.com>
2024-05-23 14:57:10 +0200

    add api

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const http = require('http');
 const port = process.env.Port || 8080;
 const host = process.env.Host || 'localhost';
 
+const apiUrl = 'https://api.chucknorris.io/jokes/random';
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type" : "text/html"});
     res.end('ciao');
@@ -10,3 +12,10 @@ const server = http.createServer((req, res) => {
 server.listen(port, host, () => {
     console.log('Server Online');
 });
+
+const fetchData = () => {
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => console.log(data.value))
+}
+fetchData();
