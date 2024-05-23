fc5e3de4943ee79de142a4e220239c2870832968
Alethepata <alessia.patane@gmail.com>
2024-05-23 15:42:26 +0200

    edit fetchData

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const host = process.env.Host || 'localhost';
 
 const apiUrl = 'https://api.chucknorris.io/jokes/random';
 
+const path = require('path');
+const fs = require('fs');
+
+const filePath = path.join(__dirname, 'norrisDb.json');
+
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type" : "text/html"});
     res.end('ciao');
@@ -13,9 +19,24 @@ server.listen(port, host, () => {
     console.log('Server Online');
 });
 
+
 const fetchData = () => {
     fetch(apiUrl)
     .then(response => response.json())
-    .then(data => console.log(data.value))
+        .then(data => {
+            fs.appendFile(filePath,
+            data.value,
+            function (err) {
+            if (err) throw err;
+            console.log("Saved!");
+            });
+        })
 }
 fetchData();
+
+
+
+
+
+
+
